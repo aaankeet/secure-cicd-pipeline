0e48787f97783fd64b3113d8f4add05d99a4b210
Ankit <thatboyankit@gmail.com>
2026-05-04 00:22:20 +0530

    Fixed Vulnerabilities

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,20 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "bad" {
-  bucket = "my-public-bucket"
+# Example of a BAD BUCKET, NO VERSIOING & NO BLOCK PUBLIC ACCESS
+# resource "aws_s3_bucket" "bad" {
+#   bucket = "my-public-bucket"
+# }
+
+# Example of a Secure Bucket
+resource "aws_s3_bucket" "secured_s3" {
+  bucket = "secured-s3-bucket"
+}
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.secured_s3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
